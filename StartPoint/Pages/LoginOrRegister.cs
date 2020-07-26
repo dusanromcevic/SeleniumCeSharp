4357ed06a151ba6756ab6c1b4db627bc28a77460
Dusan Romcevic <d.romcevic@levi9.com>
2020-07-26 23:31:54 +0200

    first commit

---
+++ StartPoint/Pages/LoginOrRegister.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace TestExercise
+{
+    public class LoginOrRegister
+    {
+        public IWebElement nekiElement => Driver.driver.FindElement(By.CssSelector());
+    }
+}
---
+++ StartPoint/StartPoint.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace StartPoint
+{
+    class StartPoint
+    {
+        static void Main()
+        {
+        }
+    }
+}
